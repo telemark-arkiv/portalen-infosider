893c2bee0de713b86ec78dda46445957032ff22b
Stian Dalen <stian@vangenplotz.no>
2016-05-02 13:01:24 +0200

    added matrix data to export json api

--- craft/config/elementapi.php
+++ craft/config/elementapi.php
@@ -8,12 +8,14 @@ return [
             'criteria' => ['section' => 'articles'],
             'transformer' => function(EntryModel $entry) {
                $arrTags = getTags($entry->tags);
+               $arrBlocks = getMatrix($entry->article_blocks);
                 return [
                     'title' => $entry->title,
                     'summary' => (string)$entry->article_intro,
                     'description' =>  (string)$entry->article_description,
                     'tags' => $arrTags,
                     'url' => $entry->url,
+                    'matrixData' => $arrBlocks,
                     'jsonUrl' => UrlHelper::getUrl("artikler/{$entry->id}.json"),
                     
                 ];
@@ -36,6 +38,11 @@ return [
         },
     ]
 ];
+/**
+ * [getTags description]
+ * @param  [type] $tags [description]
+ * @return [type]       [description]
+ */
 function getTags($tags) {
     $arrTags = array();
        foreach($tags as $tag) {
@@ -44,3 +51,61 @@ function getTags($tags) {
 
     return $arrTags;
 }
+
+/**
+ * [getMatrix description]
+ * @param  [type] $arrMatrix [description]
+ * @return [type]            [description]
+ */
+function getMatrix($arrMatrix) {
+    $arrMatrixData = array();
+    $nCounter = 0;
+    foreach ( $arrMatrix as $block) {
+       
+        if($block->type == 'textBlock') {
+            
+         //   $arrMatrixData[]['type'] = ;
+            $arrMatrixData[$nCounter]['textContent'] = strip_tags((string)$block->text);
+             $nCounter++;
+        }
+        else if($block->type == 'htmlBlock') {
+            
+         //   $arrMatrixData[]['type'] = ;
+            $arrMatrixData[$nCounter]['htmlContent'] = (string)$block->html;
+             $nCounter++;
+        }
+        else if($block->type == 'imageBlock') {
+                foreach($block->image as $image) {
+                    $strImageUrl = $image->url;
+                }
+            $arrMatrixData[$nCounter]['image'] = $strImageUrl;
+            $arrMatrixData[$nCounter]['image_text'] = (string)$block->imageText;
+             $nCounter++;
+        }
+      
+       
+    }
+
+    return $arrMatrixData;
+}
+
+/*
+  {% if block.type == 'textBlock' %}
+  {% include 'partials/_text.html' with {
+                                                          content : block.text, 
+                                                        
+                                                        } %} 
+
+                {% elseif block.type == 'htmlBlock' %}
+
+                  {% include 'partials/_text.html' with {
+                                                          content : block.html, 
+                                                        
+                                                        } %}                                           
+                {% elseif block.type == 'imageBlock' %}
+                  {% include 'partials/_image.html' with { 
+                                                          image : block.image, 
+                                                          alias : block.imagealias,
+                                                          imageText : block.imageText
+                                                        } %} 
+ */
