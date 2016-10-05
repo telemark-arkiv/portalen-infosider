3d21a285f710488336ced7dfaffcf86951d2bb69
Stian Dalen <stian@vangenplotz.no>
2016-10-05 10:21:24 +0200

    -ldap login -piwik

--- craft/plugins/tfk_portalen_ldap/controllers/Tfk_portalen_ldap_LdapController.php
+++ craft/plugins/tfk_portalen_ldap/controllers/Tfk_portalen_ldap_LdapController.php
@@ -20,19 +20,11 @@ class Tfk_portalen_ldap_LdapController extends BaseController
 	 * @return [type]
 	 */
 	public function actionLdapLogin() {
-		$strDomain = $_SERVER['HTTP_HOST'];
-		// dummly login on devserver.
-		if($strDomain === 'dev.telemarkportalen.vpdev.no')
-		{
-			
-		}
-    craft()->httpSession->add('ldapUser', true);
-      return;
+		
 		craft()->httpSession->add('loginError', false);
 
-		$strLoginName = craft()->request->getPost('loginName');
+		$strLoginName = craft()->request->getPost('loginName') . "@login.top.no";
 		$strPassword = craft()->request->getPost('password');
-				
 		$strLdapServer =craft()->tfk_portalen_ldap_api->getMySetting('ldap_url');
 		$strLdapPort= craft()->tfk_portalen_ldap_api->getMySetting('port');
 		$strBindDn = craft()->tfk_portalen_ldap_api->getMySetting('bindDn');
@@ -40,41 +32,32 @@ class Tfk_portalen_ldap_LdapController extends BaseController
 		$strSearchBase = craft()->tfk_portalen_ldap_api->getMySetting('searchBase');
 		$strSearchFilter = craft()->tfk_portalen_ldap_api->getMySetting('searchFilter');
 		$strSearchFilter = craft()->tfk_portalen_ldap_api->getMySetting('searchFilter');
-		/*$strPathSSL = craft()->tfk_portalen_ldap_api->getMySetting('pathSert');
-		//$bSSL = craft()->tfk_portalen_ldap_api->getMySetting('ssl');
 
-		if($bSSL == 'on')
-			$bSSL = true;
-		*/
 
-		ldap_set_option(NULL, LDAP_OPT_DEBUG_LEVEL, 7);
-
-		$ds = ldap_connect($strLdapServer,$strLdapPort);
+		$ldapconn = ldap_connect($strLdapServer)
+		          or die("Could not connect to {$ldaphost}");
   		
-  		$ldapbind=false;
-		if($ds) {
+  		
+		if($ldapconn) {
 			ldap_set_option($ds, LDAP_OPT_PROTOCOL_VERSION, 3);
-			ldap_set_option($ds, LDAP_OPT_REFERRALS, 0);
-
-
-			$strUserBind = str_replace("__USERNAME__", $strLoginName, $strBindDn);
-
 			
-			// binding to ldap server
-		    $ldapbind = ldap_bind($ds ,$strUserBind,$strPassword);
-		    // verify binding
-		    if ($ldapbind) {
+		    $bind = ldap_bind($ldapconn, $strBindDn, $strBindCredentials);
+		    $strSearchFilter =  str_replace("__USERNAME__",$strLoginName, $strSearchFilter);
+		   
+		    $search = ldap_search($ldapconn, $strSearchBase, $strSearchFilter) or die ("Error in search query: ".ldap_error($ldapconn));
+			$data = ldap_get_entries($ldapconn, $search);
+			
+		    if (ldap_bind($ldapconn, $strLoginName, $strPassword)){
 		    	craft()->httpSession->add('ldapUser', true);
-		      
 		    }
-		    else
-		    	craft()->httpSession->add('loginError','Feil kombinasjon av brukernavn og passord');
-		    
-		
+		    else {
+		    	craft()->httpSession->add('loginError','Feil brukernavn/passord.');
+		    	craft()->httpSession->add('ldapUser', false);
+		    }	
 		}
 		else {
 			craft()->httpSession->add('loginError','Ikke kontakt med serveren.');
 		}
-	return ;    	
+		return ;    	
 	}
 }
